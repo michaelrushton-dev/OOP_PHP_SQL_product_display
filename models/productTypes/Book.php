8345bb1f3dd383c8761537d304b4c6bd4e832a61
michaelrushton-dev <michaelwjrushton@gmail.com>
2023-02-02 23:26:36 +0000

    took out comments from book class

--- models/productTypes/Book.php
+++ models/productTypes/Book.php
@@ -1,20 +1,5 @@
+
 <?php
-// header('Access-Control-Allow-Origin: *');
-// header('Content-Type: application/json');
-// header('Access-Control-Allow-Methods: POST');
-// header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type,
-// Access-Control-Allow-Methods, Authorization, x-Requested-With');
-
-// include_once '../Product.php';
-// include_once '../../db/Database.php';
-
-
-// $database = new Database();
-// $db = $database->connect();
-
-
-// $data = json_decode(file_get_contents("php://input"));
-
 class Book extends Product {
 
   public function talk()
@@ -23,36 +8,5 @@ class Book extends Product {
     echo 'I am a Book';
   }
 }
-
-// $newBook = new Book($db);
-// $newBook->setType('Book');
-// $newBook->talk();
-
-// // print_r($data);
-
-// $newBook->sku = $data->sku;
-// $newBook->name = $data->name;
-// $newBook->price = $data->price;
-// //type of is added by SETTER
-// $newBook->value = $data->value;
-
-// print_r($newBook);
-
-
-// if($newBook->create()){
-//   echo json_encode(
-//     array('message' => 'Product Created!')
-//   );
-// } else {
-//   echo json_encode(
-//     array('message' => 'Product not created')
-//   );
-// }
-
-
-
-
-
-
 
 
