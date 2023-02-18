cf58fdd7031da99733efd5e57a1332f9d3c5ca14
michaelrushton-dev <michaelwjrushton@gmail.com>
2023-02-18 16:27:03 +0000

    branched off

--- index.php
+++ index.php
@@ -1,23 +1,58 @@
 <?php
 
-require_once __DIR__ . '/vendor/autoload.php';
-require_once __DIR__ . '/config/config.php';
+//runs the script to actually fetch all of the data
+//Headers
 
-// use app\core\Router;
-// use app\controllers\ProductController;
-// use app\core\Database;
+include_once './db/Database.php';
+include_once './models/Product.php';
+class Controller{
 
-require_once('./db/Database.php');
-require_once('./Router/Router.php');
+  public static function Get(){
+    $database = new Database();
+    $db = $database->connect();
+    //Instantiate a new product object from Product class
+    $product = new Product($db);
+    // Blog product query
+    $result = $product->read();
+    $num = $result->rowCount();
+    
+    //check if any products
+    if($num>0){
+      //product array
+      $products_arr = array();
+      while($row = $result->fetch(PDO::FETCH_ASSOC)){
+        extract($row);
+        $product_item = array(
+          'id' => $id,
+          'sku'=>$sku,
+          'name'=>$name,
+          'price'=> $price,
+          'type'=> $type,
+          'value'=> $value,
+          'size'=> $size,
+          'weight'=> $weight,
+          'dimensions'=> $dimensions,
+        );
+        //push to 'data'
+        array_push($products_arr, $product_item);
+      }
+      //turn to json and output
+      echo json_encode($products_arr);
+    }else{
+      //no products
+      echo json_encode(
+        array('message' => ' '. $num .' products found')
+      );
+    }
+  }
 
-include_once('./Get.php');
+// header('Access-Control-Allow-Origin: *');
+// header('Access-Control-Allow-Origin: http://127.0.0.1:5173');
+// header('Content-Type: application/json');
+// header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type,
+// Access-Control-Allow-Methods, Authorization, x-Requested-With');
+//brings in Database and product classes
 
-$database = new Database();
-$router = new Router();
+// Instantiate DB and connect to it
 
-$router->get('/', [Controller::class, 'Get']);
-// $router->post('/add-product', [ProductController::class, 'create']);
-// $router->post('/delete-product', [ProductController::class, 'delete']);
-// $router->get('/products', [ProductController::class, 'read']);
-
-$router->run();
+}
