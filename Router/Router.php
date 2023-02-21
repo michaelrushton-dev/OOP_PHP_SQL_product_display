1fc2473608108aa9e9c4b00a94a92e57099998fc
michaelrushton-dev <michaelwjrushton@gmail.com>
2023-02-21 13:13:39 +0000

    methods

--- Router/Router.php
+++ Router/Router.php
@@ -21,7 +21,7 @@ class Router
         header("Content-Type: application/json; charset=UTF-8");
         header('Access-Control-Allow-Origin: *');
         header("Access-Control-Allow-Headers: *");
-        header("Access-Control-Allow-Methods: *");
+        header("Access-Control-Allow-Methods: GET, POST, DELETE");
         header("Allow: *");
 
         //get the request uri from the request
