56d3105a036635d6171a40aeae06b2aa44897c5e
michaelrushton-dev <michaelwjrushton@gmail.com>
2023-02-21 14:37:03 +0000

    data is array

--- Controller.php
+++ Controller.php
@@ -61,7 +61,7 @@ $db = $database->connect();
 $data = json_decode(file_get_contents("php://input"));
 
 // new product called whatever type is
-
+// print_r($data);
 // $product = new $data->type($db);
 // $product->talk();
 
@@ -95,7 +95,7 @@ $data = json_decode(file_get_contents("php://input"));
 // }
 
 echo json_encode(
-      $data
+      array($data)
     );
 
 }
