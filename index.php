773fa674753e771df4d136b64e5473b3ff397b45
michaelrushton-dev <michaelwjrushton@gmail.com>
2023-02-22 12:25:13 +0000

    removed api folder

--- index.php
+++ index.php
@@ -1,12 +1,5 @@
 <?php
 
-// require_once __DIR__ . '/vendor/autoload.php';
-// require_once __DIR__ . '/config/config.php';
-
-// use app\core\Router;
-// use app\controllers\ProductController;
-// use app\core\Database;
-
 require_once('./db/Database.php');
 require_once('./Router/Router.php');
 require_once('./Controller.php');
